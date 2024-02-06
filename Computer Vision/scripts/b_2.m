190a3ac2c5249b9b72cef280a9c6c76c042f092f
Berk-Bilgin <158227431+Berk-Bilgin@users.noreply.github.com>
2024-02-06 14:34:04 +0000

    Update b_2.m

--- Computer Vision/scripts/b_2.m
+++ Computer Vision/scripts/b_2.m
@@ -1,12 +1,15 @@
 load("/MATLAB Drive/F0_Electrodes.mat");
 ElectrodeData = cell2mat(dataVectors_electrodes);
 [coeff, score, latent] = pca(ElectrodeData);
+figure(1); 
+subplot(2, 1, 1);
 plot(latent, '-o')
 title("Scree Plot")
 xticks(1:2:19)
 xlabel("Principal Components")
 ylabel("Variance")
-figure
+%figure
+subplot(2, 1, 2);
 scatter3(score(:,1), score(:,2), score(:,3), 'filled')
 xlabel("PC1")
 ylabel("PC2")
