b2bfa8a0b71eac264320897adaaa43a9ee5ac207
Berk-Bilgin <158227431+Berk-Bilgin@users.noreply.github.com>
2024-02-07 17:44:14 +0000

    Update b_2.m

--- Computer Vision/scripts/b_2.m
+++ Computer Vision/scripts/b_2.m
@@ -1,6 +1,12 @@
 load("/MATLAB Drive/F0_Electrodes.mat");
 ElectrodeData = cell2mat(dataVectors_electrodes);
 [coeff, score, latent] = pca(ElectrodeData);
+colours = [0 0 1; %blue = acrylic
+           0 0 0; %black = black foam
+           1 1 0; %yellow = car sponge
+           0.5 0 0.5; %purple = flour sack 
+           0 1 0; %green = kitchen sponge
+           1 0 0]; %red = steel vase
 figure(1); 
 subplot(2, 1, 1);
 plot(latent, '-o')
@@ -8,9 +14,11 @@ title("Scree Plot")
 xticks(1:2:19)
 xlabel("Principal Components")
 ylabel("Variance")
-%figure
 subplot(2, 1, 2);
-scatter3(score(:,1), score(:,2), score(:,3), 'filled')
+for i=0:5
+    scatter3(score(i*10+1:(i+1)*10,1), score(i*10+1:(i+1)*10,2), score(i*10+1:(i+1)*10), 20, colours(i+1, :), 'filled')
+    hold on;
+end
 xlabel("PC1")
 ylabel("PC2")
 zlabel("PC3")
